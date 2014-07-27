04879d783d55bbf8b09308b0b4c9e7c7125b80ca
tochange <tochange2011@gmail.com>
2014-07-27 20:44:43 +0800

    add .so

---
+++ SavigationLib/obj/local/armeabi/objs/phone-number-jni/number-jni.o.d
@@ -0,0 +1,71 @@
+E:/myworkspace/savigation/SavigationLib/obj/local/armeabi/objs/phone-number-jni/number-jni.o: \
+ E:/myworkspace/savigation/SavigationLib/jni/number-jni.cpp \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/jni.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/string.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/malloc.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdio.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdint.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/alloca.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/strings.h \
+ E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/memory.h \
+ E:/myworkspace/savigation/SavigationLib/jni/NumberInfo.h
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/jni.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/string.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdio.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdlib.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/strings.h:
+
+E:/Android_SDK/android-ndk-r7-windows/android-ndk-r7/platforms/android-14/arch-arm/usr/include/memory.h:
+
+E:/myworkspace/savigation/SavigationLib/jni/NumberInfo.h:
